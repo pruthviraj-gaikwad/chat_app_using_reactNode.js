685002a7c19b9180f15c7b8c7b1ff17709bb1a69
pruthviraj-gaikwad <pruthviraj162004@gmail.com>
2025-04-11 16:17:39 +0530

    Chat Application

---
+++ public/src/components/Welcome.jsx
@@ -0,0 +1,51 @@
+
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+
+export default function Welcome() {
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    const fetchUserName = async () => {
+      const data = await JSON.parse(
+        localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+      );
+      setUserName(data.username);
+    };
+
+    fetchUserName();
+  }, []);
+
+  return (
+    <Container>
+      <img src={Robot} alt="Robot welcome" />
+      <h1>
+        Welcome, <span>{userName}!</span>
+      </h1>
+      <h3>Please select a chat to start messaging.</h3>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  text-align: center;
+
+  img {
+    height: 20rem;
+  }
+
+  span {
+    color: #4e0eff;
+  }
+
+  h1,
+  h3 {
+    margin: 0.5rem 0;
+  }
+`;
